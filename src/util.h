e05d3af8532d7c8d3d08c887451130d4c9f8d6f3
MFaik <m.faikekclr@gmail.com>
2024-08-04 14:45:31 +0300

    changed chunk hashing algorithm

--- src/util.h
+++ src/util.h
@@ -4,6 +4,8 @@
 template<>
 struct std::hash<std::array<int, 2>> {
     std::size_t operator()(const std::array<int, 2>& a) const noexcept {
-        return (a[0] + (324723947 + a[1])) ^93485734985;
+        //TODO: profile this
+        return (size_t)a[0] + (size_t)a[1];
+        // return (a[0] + (324723947 + a[1])) ^93485734985;
     }
 };
