436ac901e13c34f7ec1bad1b55ba2bf4aefe8d3e
MFaik <m.faikekclr@gmail.com>
2024-08-09 00:02:08 +0300

    split program parser into functions

--- src/program_parser.cpp
+++ src/program_parser.cpp
@@ -81,7 +81,145 @@ struct ProgramError : public std::runtime_error {
     using std::runtime_error::runtime_error;
 };
 
-//TODO: add error handling
+std::string parse_label(
+        std::unordered_map<std::string, int> &label_map, 
+        Program &ret,
+        std::string &line,
+        int &code_cnt) {
+    int l = 0, r = line.size();
+    //remove comments
+    for(int i = l;i < r;i++) {
+        if(line[i] == '#')
+            r = i;
+    }
+    while(l < r && isspace(line[l]))l++;
+    while(l < r && isspace(line[r-1]))r--;
+    if(l == r)return "";
+
+    if(line[r-1] == ':') {
+        if(r-l == 1)
+            return "label cannot be empty";
+        std::string label = line.substr(l, r-l);
+        if(label.find(' ') != std::string::npos)
+            return "label cannot have spaces: " + label;
+        if(constant_map.find(label) != constant_map.end())
+            return "label cannot have the same name as a constant: " + label;
+        if(reg_map.find(label) != reg_map.end())
+            return "label cannot have the same name as a register: " + label;
+        if(label_map.find(label) != label_map.end())
+            return "two labels cannot have the same name: " + label;
+
+        label_map[line.substr(l, r-l-1)] = ret._labels.size();
+        ret._labels.push_back(code_cnt);
+    } else {
+        code_cnt++;
+    }
+    return "";
+}
+
+std::string parse_instruction(
+        std::unordered_map<std::string, int> &label_map, 
+        Program &ret,
+        std::string &line
+        ) {
+    int l = 0, r = line.size();
+    //remove comments
+    for(int i = l;i < r;i++) {
+        if(line[i] == '#')
+            r = i;
+    }
+    while(l < r && isspace(line[l]))l++;
+    while(l < r && isspace(line[r-1]))r--;
+    if(l == r)return "";
+
+    //skip labels
+    if(line[r-1] == ':') 
+        return "";
+
+    Program::Instruction ins;
+
+    for(int i = l;i <= r;i++) {
+        if(isspace(line[i]) || i == r) {
+            auto op = op_map.find(line.substr(l, i-l));
+            if(op == op_map.end()) {
+                return "unkown op: " + line.substr(l, i-l);
+            }
+            ins.op = op->second;
+            l = i;
+            break;
+        }
+    }
+    while(l < r && isspace(line[l]))l++;
+    int oc = operand_cnt.at(ins.op);
+    if(oc == 2) {
+        if(l == r) {
+            return "expected two operands";
+        }
+        for(int i = l;i <= r;i++) {
+            if(isspace(line[i]) || i == r) {
+                auto reg = reg_map.find(line.substr(l, i-l));
+                if(reg == reg_map.end())
+                    return "unkown register: " + line.substr(l, i-l);
+
+                ins.reg = reg->second;
+                l = i;
+                break;
+            }
+        }
+    }
+    while(l < r && isspace(line[l]))l++;
+    if(oc >= 1) {
+        if(l == r)
+            return oc == 2 ? "expected two operands" : "expected one operand";
+
+        bool isnum = true;
+        for(int i = l;i <= r;i++) {
+            if(isspace(line[i]) || i == r) {
+                std::string o = line.substr(l, i-l);
+                auto reg = reg_map.find(o);
+                if(reg != reg_map.end()) {
+                    ins.register_toggle = true;
+                    ins.operand = reg->second;
+                } else {
+                    ins.register_toggle = false;
+                    if(isnum) {
+                        try {
+                            ins.operand = stoi(o);
+                        } catch(...) {
+                            return "invalid operand: " + o;
+                        }
+                    } else {
+                        auto c = constant_map.find(o);
+                        if(c == constant_map.end())
+                            return "invalid operand: " + o;
+                        ins.operand = c->second;
+                    }
+                }
+                break;
+            } else {
+                isnum &= isdigit(line[i]);
+            }
+        }
+    }
+
+    if(ins.op == Program::JMP || ins.op == Program::JNE 
+            || ins.op == Program::JEQ) {
+        for(int i = l;i <= r;i++) {
+            if(isspace(i) || i == r) {
+                auto label = label_map.find(line.substr(l, r-l));
+                if(label == label_map.end()) 
+                    return "unknown label: " + line.substr(l, r-l);
+
+                ins.operand = label->second;
+                break;
+            }
+        }
+    }
+
+    ret._code.push_back(ins);
+    return "";
+}
+
 Program parse_program(std::string str) {
     //the code must be all caps
     std::transform(str.begin(), str.end(), str.begin(), toupper);
@@ -91,45 +229,17 @@ Program parse_program(std::string str) {
     std::string line;
 
     std::unordered_map<std::string, int> label_map;
-    
+
     int line_cnt = -1;
-    int code_cnt = 0;
     //parse the labels first to allow forward jumps
+    int code_cnt = 0;
     while(std::getline(f, line)) {
         line_cnt++;
-        try{
-            int l = 0, r = line.size();
-            //remove comments
-            for(int i = l;i < r;i++) {
-                if(line[i] == '#')
-                    r = i;
-            }
-            while(l < r && isspace(line[l]))l++;
-            while(l < r && isspace(line[r-1]))r--;
-            if(l == r)continue;
-
-            if(line[r-1] == ':') {
-                if(r-l == 1)
-                    throw ProgramError("label cannot be empty");
-                std::string label = line.substr(l, r-l);
-                if(label.find(' ') != std::string::npos)
-                    throw ProgramError("label cannot have spaces: " + label);
-                if(constant_map.find(label) != constant_map.end())
-                    throw ProgramError("label cannot have the same name as a constant: " + label);
-                if(reg_map.find(label) != reg_map.end())
-                    throw ProgramError("label cannot have the same name as a register: " + label);
-                if(label_map.find(label) != label_map.end())
-                    throw ProgramError("two labels cannot have the same name: " + label);
-
-                label_map[line.substr(l, r-l-1)] = ret._labels.size();
-                ret._labels.push_back(code_cnt);
-            } else {
-                code_cnt++;
-            }
-        } catch(const ProgramError &err) {
+        std::string err = parse_label(label_map, ret, line, code_cnt);
+        if(err.size()) {
             ret._errors.push_back({
                 line_cnt,
-                err.what(),
+                err,
             });
         }
     }
@@ -139,107 +249,11 @@ Program parse_program(std::string str) {
     line_cnt = -1;
     while(std::getline(f, line)) {
         line_cnt++;
-        try {
-            int l = 0, r = line.size();
-            //remove comments
-            for(int i = l;i < r;i++) {
-                if(line[i] == '#')
-                    r = i;
-            }
-            while(l < r && isspace(line[l]))l++;
-            while(l < r && isspace(line[r-1]))r--;
-            if(l == r)continue;
-
-            //skip labels
-            if(line[r-1] == ':') {
-                continue;
-            }
-
-            Program::Instruction ins;
-
-            for(int i = l;i <= r;i++) {
-                if(isspace(line[i]) || i == r) {
-                    auto op = op_map.find(line.substr(l, i-l));
-                    if(op == op_map.end()) {
-                        throw ProgramError("unkown op: " + line.substr(l, i-l));
-                    }
-                    ins.op = op->second;
-                    l = i;
-                    break;
-                }
-            }
-            while(l < r && isspace(line[l]))l++;
-            int oc = operand_cnt.at(ins.op);
-            if(oc == 2) {
-                if(l == r) {
-                    throw ProgramError("expected two operands");
-                }
-                for(int i = l;i <= r;i++) {
-                    if(isspace(line[i]) || i == r) {
-                        auto reg = reg_map.find(line.substr(l, i-l));
-                        if(reg == reg_map.end())
-                            throw ProgramError("unkown register: " + line.substr(l, i-l));
-
-                        ins.reg = reg->second;
-                        l = i;
-                        break;
-                    }
-                }
-            }
-            while(l < r && isspace(line[l]))l++;
-            if(oc >= 1) {
-                if(l == r)
-                    throw ProgramError(oc == 2 ? "expected two operands" : "expected one operand");
-                
-                bool isnum = true;
-                for(int i = l;i <= r;i++) {
-                    if(isspace(line[i]) || i == r) {
-                        std::string o = line.substr(l, i-l);
-                        auto reg = reg_map.find(o);
-                        if(reg != reg_map.end()) {
-                            ins.register_toggle = true;
-                            ins.operand = reg->second;
-                        } else {
-                            ins.register_toggle = false;
-                            if(isnum) {
-                                try {
-                                    ins.operand = stoi(o);
-                                } catch(...) {
-                                    throw ProgramError("invalid operand: " + o);
-                                }
-                            } else {
-                                auto c = constant_map.find(o);
-                                if(c == constant_map.end())
-                                    throw ProgramError("invalid operand: " + o);
-                                ins.operand = c->second;
-                            }
-                        }
-                        break;
-                    } else {
-                        isnum &= isdigit(line[i]);
-                    }
-                }
-            }
-
-            if(ins.op == Program::JMP || ins.op == Program::JNE 
-                    || ins.op == Program::JEQ) {
-                for(int i = l;i <= r;i++) {
-                    if(isspace(i) || i == r) {
-                        auto label = label_map.find(line.substr(l, r-l));
-                        if(label == label_map.end()) 
-                            throw ProgramError("unknown label: " + line.substr(l, r-l));
-
-                        ins.operand = label->second;
-                        break;
-                    }
-                }
-            }
-
-            ret._code.push_back(ins);
-        } catch(const ProgramError &err) {
+        std::string err = parse_instruction(label_map, ret, line);
+        if(err.size()) {
             ret._errors.push_back({
                 line_cnt,
-                err.what(),
+                err,
             });
         }
     }
